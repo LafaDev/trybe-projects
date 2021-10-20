aa24e5255d0a832b908e28354fc2194f857572eb
lafa <lafa.dev@protonmail.com>
2021-10-20 15:17:11 -0400

    primerio dia do quinto bloco

---
+++ Fundamentals/05-JavaScript-DOM-Web-Storage/day-01-DOM-Selectors/contents/part-II/script.js
@@ -0,0 +1,8 @@
+document.getElementById('page-title').innerText = 'The Revenant';
+document.getElementById('paragraph').style.backgroundColor = "black";
+document.getElementById('subtitle').innerText = '2015 film';
+document.getElementById('second-paragraph').style.fontSize = '15px';
+
+document.getElementsByClassName('exParagraph');
+document.getElementsByClassName('exParagraph')[0].style.color = 'white';
+document.getElementsByTagName('h4')[0].style.color = 'blue';
---
+++ Fundamentals/05-JavaScript-DOM-Web-Storage/day-01-DOM-Selectors/contents/part-III/script.js
@@ -0,0 +1,9 @@
+document.querySelector('#header-container').style.backgroundColor = 'darkseagreen';
+document.querySelector('#footer-container').style.backgroundColor = 'black';
+document.querySelector('.no-emergency-tasks').style.backgroundColor = "green";
+document.querySelectorAll('.no-emergency-tasks div h3')[0].style.backgroundColor = "black";
+document.querySelectorAll('.no-emergency-tasks div h3')[1].style.backgroundColor = "black";
+
+document.querySelector('.emergency-tasks').style.backgroundColor = "orange";
+document.querySelectorAll('.emergency-tasks div h3')[0].style.backgroundColor = "blue";
+document.querySelectorAll('.emergency-tasks div h3')[1].style.backgroundColor = "blue";
